be9ffb83725a24a94ef2b1f2cf1f17b21278b159
Maksimka <138521714+04Maksimka@users.noreply.github.com>
2026-01-11 15:30:04 +0300

    add messier game

--- messier/messier_example.py
+++
@@ -1,2 +0,0 @@
-#TODO: modify stereographic_projection and pinhole_projection and cylindric_projection so that they will be
-# able to plot messier objects with their  Messier number and create this examples here
