65236fec15da7ae3641a5a8ab2c0e6f0f7fef308
shroomjak <ilya2982004@gmail.com>
2026-02-26 11:12:57 +0300

    patch for sphinx click sphinx.ext.autodoc -> sphinx.ext.autodoc.mock

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -2,6 +2,14 @@
 import sys
 from pathlib import Path
 
+import sphinx.ext.autodoc as _autodoc
+
+# Patch for sphinx-click: import mock in Sphinx >= 9.0
+import sphinx.ext.autodoc.mock as _mock_module
+
+if not hasattr(_autodoc, "mock"):
+    _autodoc.mock = _mock_module.mock
+
 from sphinx_pyproject import SphinxConfig
 
 html_theme = "furo"
