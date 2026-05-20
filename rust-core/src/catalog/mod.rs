d0011341d0282515ac2a343f4e0f40fd448824de
vobolgus <svyatoslav.souglobov@gmail.com>
2026-05-20 12:34:05 +0300

    Revert "Merge pull request #5 from vobolgus/feat/rust-backend-core"
    This reverts commit 0c2e2ed184565e5e2f468d6a6bfa94c73189120b, reversing
    changes made to a799b133e66fabf3cf4069363fe5c6a3209efddf.

--- rust-core/src/catalog/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod hip;
-pub mod messier;
-
-pub use hip::{HipCatalog, Star};
-pub use messier::{MessierCatalog, MessierObject};
--- rust-core/src/projections/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod pinhole;
-pub mod stereo;
-
-pub use pinhole::{CameraConfig, PinholeProjection, PinholeProjectionResult};
-pub use stereo::{StereoProjection, StereoProjectionResult};
